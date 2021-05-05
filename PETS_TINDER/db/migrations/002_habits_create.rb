3db870ffa01747b8218d99cc6e259ef28d2235e1
Ed <edgar.duronr@gmail.com>
2021-05-06 00:43:32 +0800

    hardening migrations, all columns

--- PETS_TINDER/db/migrations/002_habits_create.rb
+++ PETS_TINDER/db/migrations/002_habits_create.rb
@@ -5,9 +5,9 @@ Sequel.migration do
     create_table(:habits) do
       primary_key :id
 
-      String :name, null: false
-      String :category, null: false
-      String :description, null: false, defaulf: ''
+      String :name_secure, null: false
+      String :category_secure, null: false
+      String :description_secure, null: false, defaulf: ''
 
       DateTime :created_at
       DateTime :updated_at
