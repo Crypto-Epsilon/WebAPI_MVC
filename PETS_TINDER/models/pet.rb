9b7e15d1f934084d1c8c4c4f04ac0bd3c659744d
Ed <edgar.duronr@gmail.com>
2021-05-06 01:20:59 +0800

    added encrypt/decrypt models

--- PETS_TINDER/models/pet.rb
+++ PETS_TINDER/models/pet.rb
@@ -8,7 +8,38 @@ module Pets_Tinder
 
         plugin :timestamp
 
-        
+        #Secure getter and setters
+        def petname
+            SecureDB.decrypt(petname_secure)
+        end
+
+        def petname
+            self.petname_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def petrace
+            SecureDB.decrypt(petrace_secure)
+        end
+
+        def petrace
+            self.petrace_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def birthday
+            SecureDB.decrypt(birthday_secure)
+        end
+
+        def birthday
+            self.birthday_secure = SecureDB.encrypt(plaintext)
+        end
+
+        def description
+            SecureDB.decrypt(description_secure)
+        end
+
+        def description
+            self.description_secure = SecureDB.encrypt(plaintext)
+        end
 
         def to_json(options= {})
             JSON(
