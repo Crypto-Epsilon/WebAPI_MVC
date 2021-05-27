9c7e56b6175e5f2d67f36e1161533b1a18c00a58
Malherbe <malherbe.billy@gmail.com>
2021-05-27 14:42:44 +0800

    ADD DATABASE POSTGRE

--- config/environments.rb
+++ config/environments.rb
@@ -11,49 +11,29 @@ module Pets_Tinder
   class Api < Roda
     plugin :environments
 
-    Figaro.application = Figaro::Application.new(
-      environment: environment,
-      path: File.expand_path('config/secrets.yml')
-    )
-    Figaro.load
+    # rubocop:disable Lint/ConstantDefinitionInBlock
+    configure do
+      # Environment variables setup
+      Figaro.application = Figaro::Application.new(
+        environment: environment,
+        path: File.expand_path('config/secrets.yml')
+      )
+      Figaro.load
+      def self.config() = Figaro.env
 
-    # Make the environment variables accessible to other classes
-    def self.config
-      Figaro.env
+      # Logger setup
+      LOGGER = Logger.new($stderr)
+      def self.logger() = LOGGER
+
+      # Database Setup
+      DB = Sequel.connect(ENV.delete('DATABASE_URL'))
+      def self.DB() = DB 
     end
+    
 
-    # Logger setup
-    LOGGER = Logger.new($stderr)
-    def self.logger() = LOGGER
-
-    #I'm a little confused here, not sure if we still need to utilize the self.reload model
     configure :development, :test do
       require 'pry'
-        # Allows running reload! in pry to restart entire app
-        def self.reload!
-          exec 'pry -r ./specs/test_load_all'
-        end
-        logger.level = Logger::ERROR
-      end
-  
-      configure :development, :test do
-        ENV['DATABASE_URL'] = 'sqlite://' + config.DB_FILENAME
-      end
-
-      configure :production do
-        #Not sure yet
-      end
-  
-      # For all environments, should we keep it here?
-      configure do
-        DB = Sequel.connect(ENV.delete['DATABASE_URL'])
-
-        # Make the database accessible to other classes
-        def self.DB() = DB # rubocop:disable Naming/MethodName
-        end
-
-        SecureDB.setup(config.DB_KEY)
-
-      end
+      logger.level = Logger::ERROR
+    end
   end
 end
