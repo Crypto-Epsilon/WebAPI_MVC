4f10533447e1050c19610867161cd21989db8498
Malherbe <malherbe.billy@gmail.com>
2021-06-21 18:22:38 +0800

    Require routes without aut_token

--- Rakefile
+++ Rakefile
@@ -102,16 +102,26 @@ end
 
 # We can use this rake to create new keys for dev, test, prod (we can have different ones)
 namespace :newkey do
+  task(:load_libs) {require_app 'lib'}
+
   desc 'Create sample cryptographic key for database'
-  task :db do
+  task :db => :load_libs do
     require_app('lib')
     puts "DB_KEY: #{SecureDB.generate_key}"
   end
 
   desc 'Create sample cryptographic key for tokens and messaging'
-  task :msg do
+  task :msg => :load_libs do
     require_app('lib')
     puts "MSG_KEY: #{AuthToken.generate_key}"
+  end
+
+  desc 'Create sample sign/verify keypair for signed communication'
+  task :signing => :load_libs do
+    keypair = SignedRequest.generate_keypair
+
+    puts "SIGNING_KEY: #{keypair[:signing_key]}"
+    puts " VERIFY_KEY: #{keypair[:verify_key]}"
   end
 end
 
--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,11 +7,17 @@ module PetsTinder
   # Web controller for Credence API
   class Api < Roda
     route('auth') do |routing|
+      # All requests in this route require signed requests
+      begin
+        @request_data = SignedRequest.new(Api.config).parse(request.body.read)
+      rescue SignedRequest::VerificationError
+        routing.halt '403', { message: 'Must sign request' }.to_json
+      end
+
       routing.on 'register' do
         # POST /api/v1/auth/authenticate
         routing.post do
-          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
-          VerifyRegistration.new(reg_data).call
+          VerifyRegistration.new(@request_data).call
 
           response.status = 202
           { message: 'Verification email sent' }.to_json
@@ -26,8 +32,7 @@ module PetsTinder
 
       routing.is 'authenticate' do
         routing.post do
-          credentials = JsonRequestBody.parse_symbolize(request.body.read)
-          auth_account = AuthenticateAccount.call(credentials)
+          auth_account = AuthenticateAccount.call(@request_data)
           { data: auth_account }.to_json
         rescue AuthenticateAccount::UnauthorizedError => e
           puts [e.class, e.message].join ': '
@@ -37,9 +42,7 @@ module PetsTinder
 
        # POST /api/v1/auth/sso
        routing.post 'sso' do
-        auth_request = JsonRequestBody.parse_symbolize(request.body.read)
-
-        auth_account = AuthorizeSso.new.call(auth_request[:access_token])
+        auth_account = AuthorizeSso.new.call(@request_data[:access_token])
         { data: auth_account }.to_json
       rescue StandardError => error
         puts "FAILED to validate Github account: #{error.inspect}"
